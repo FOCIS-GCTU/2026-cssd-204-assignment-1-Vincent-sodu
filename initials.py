061b26a5a7fc4a87a2a448afac96df89ad94f15a
Vincent-sodu <2425402482@live.gctu.edu.gh>
2026-05-31 21:47:22 +0000

    complete assignment and task

--- initials.py
+++ initials.py
@@ -1,7 +1,32 @@
-
+# File: initials.py
+# Description: Prints my initials VKM in standard and stylized  large formats.
+# Assignment Number: 1
+#
+# Name: VINCENT KWAKYE MENSAH
+# STUDENT ID: 2425402882
+# Email: 2425402482@live.gctu.edu.gh
+# Grader:Augustus Buckman
+#
+# On my honor,VINCENT KWAKYE MENSAH , this programming assignment is my own work
+# and I have not provided this code to any other student.
 def main():
-  pass #code goes here
+     # This program prints out my initials in standard and stylized formats.
+    print()
+    print("...VKM")
+    print()
 
+    print("...VV........VV........KK.....KK.........MM........MM.....")
+    print("...VV........VV........KK....KK..........MMM......MMM.....")
+    print("....VV......VV.........KK...KK...........MMMM....MMMM.....")
+    print("....VV......VV.........KK..KK............MM.MM..MM.MM.....")
+    print(".....VV....VV..........KKKKK.............MM..MMMM..MM.....")
+    print(".....VV....VV..........KK..KK............MM..MMMM..MM.....")
+    print("......VV..VV...........KK...KK...........MM...MM...MM.....")
+    print("......VV..VV...........KK....KK..........MM........MM.....")
+    print(".......VVVV............KK.....KK.........MM........MM.....")
+    print("........VV........**...KK......KK...**...MM........MM...**")
+
+    print()
 
 
 main()
